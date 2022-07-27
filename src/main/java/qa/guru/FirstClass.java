4690dc67f0c67a7b0f2ff5326b379c58cdab3fd8
Berezyak Vadim <newberez@gmail.com>
2022-07-27 15:20:00 +0300

    first commit

---
+++ src/main/java/qa/guru/FirstClass.java
@@ -0,0 +1,8 @@
+package qa.guru;
+
+public class FirstClass {
+    public static void main(String[] args) {
+    StudentQAguru student = new StudentQAguru("Ivan",21, 8,8,2);
+    student.infoAboutStudent(student);
+    }
+}
---
+++ src/main/java/qa/guru/StudentQAguru.java
@@ -0,0 +1,22 @@
+package qa.guru;
+
+public class StudentQAguru {
+    String name;
+    int age;
+    int workTime;
+    int sleepTime;
+    int studyTime;
+
+    public StudentQAguru(String name, int age, int workTime, int sleepTime, int studyTime) {
+        this.name = name;
+        this.age = age;
+        this.workTime = workTime;
+        this.sleepTime = sleepTime;
+        this.studyTime = studyTime;
+    }
+
+    public void infoAboutStudent(StudentQAguru student){
+        System.out.println(student.name);
+
+    }
+}
