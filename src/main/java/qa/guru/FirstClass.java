8d701162fb55bf68209dc8850e80b3e628ef249d
Berezyak Vadim <newberez@gmail.com>
2022-07-27 15:35:26 +0300

    info method was modified

--- src/main/java/qa/guru/FirstClass.java
+++ src/main/java/qa/guru/FirstClass.java
@@ -2,7 +2,7 @@ package qa.guru;
 
 public class FirstClass {
     public static void main(String[] args) {
-    StudentQAguru student = new StudentQAguru("Ivan",21, 8,8,2);
+    StudentQAguru student = new StudentQAguru("Ivan",21, 8,8,1);
     student.infoAboutStudent(student);
     }
 }
--- src/main/java/qa/guru/StudentQAguru.java
+++ src/main/java/qa/guru/StudentQAguru.java
@@ -3,20 +3,27 @@ package qa.guru;
 public class StudentQAguru {
     String name;
     int age;
-    int workTime;
-    int sleepTime;
-    int studyTime;
+    int DayWorkTime;
+    int DaySleepTime;
+    int DayStudyTime;
 
     public StudentQAguru(String name, int age, int workTime, int sleepTime, int studyTime) {
         this.name = name;
         this.age = age;
-        this.workTime = workTime;
-        this.sleepTime = sleepTime;
-        this.studyTime = studyTime;
+        this.DayWorkTime = workTime;
+        this.DaySleepTime = sleepTime;
+        this.DayStudyTime = studyTime;
     }
 
-    public void infoAboutStudent(StudentQAguru student){
-        System.out.println("Name: " + student.name + "," +" Age: " + student.age);
+    public void infoAboutStudent(StudentQAguru student) {
+        System.out.println("Name: " + student.name + "," + " Age: " + student.age);
+        if (DayStudyTime <= 1) {
+            System.out.println("You should increase your studying time!");
+        } else if (DayStudyTime >= 4) {
+            System.out.println("Hold on buddy! Your head can boiled!");
+        } else {
+            System.out.println("Your studying time is OK! Good job!");
+        }
 
     }
 }
