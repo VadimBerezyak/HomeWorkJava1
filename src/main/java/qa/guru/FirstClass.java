bfc6ca2405fe2b726372cea809327bde87431a47
Berezyak Vadim <newberez@gmail.com>
2022-07-27 16:02:37 +0300

    final commit

--- src/main/java/qa/guru/FirstClass.java
+++ src/main/java/qa/guru/FirstClass.java
@@ -2,7 +2,10 @@ package qa.guru;
 
 public class FirstClass {
     public static void main(String[] args) {
-    StudentQAguru student = new StudentQAguru("Ivan",21, 8,8,1);
-    student.infoAboutStudent(student);
+    StudentQAguru student = new StudentQAguru("Ivan",21, 8,8,6);
+    student.studyingAnalysisOfStudent(student);
+    student.workingAnalysisOfStudent(student);
+    student.sleepingAnalysisOfStudent(student);
+    student.freeTimeOfStudent(student);
     }
 }
--- src/main/java/qa/guru/StudentQAguru.java
+++ src/main/java/qa/guru/StudentQAguru.java
@@ -15,15 +15,49 @@ public class StudentQAguru {
         this.DayStudyTime = studyTime;
     }
 
-    public void infoAboutStudent(StudentQAguru student) {
+    public void studyingAnalysisOfStudent(StudentQAguru student) {
         System.out.println("Name: " + student.name + "," + " Age: " + student.age);
         if (DayStudyTime <= 1) {
             System.out.println("You should increase your studying time!");
-        } else if (DayStudyTime >= 4) {
+            student.DayStudyTime += 1;
+        } else if (DayStudyTime > 4) {
             System.out.println("Hold on buddy! Your head can boiled!");
+            student.DayStudyTime = 4;
         } else {
             System.out.println("Your studying time is OK! Good job!");
         }
 
     }
-}
+
+    public void workingAnalysisOfStudent(StudentQAguru student) {
+        System.out.println("Name: " + student.name + "," + " Age: " + student.age);
+        if (DayWorkTime < 8) {
+            System.out.println("You should increase your work time to earn more money!");
+            student.DayStudyTime = 8;
+        } else if (DayStudyTime > 8) {
+            System.out.println("Hold on buddy! Your head can boiled!");
+            student.DayStudyTime = 8;
+        } else {
+            System.out.println("Your working time is OK! Good job!");
+        }
+    }
+    public void sleepingAnalysisOfStudent (StudentQAguru student) {
+            System.out.println("Name: " + student.name + "," + " Age: " + student.age);
+            if (DaySleepTime < 8) {
+                System.out.println("You should increase your sleep time to have a good rest!");
+                student.DaySleepTime = 8;
+            } else if (DaySleepTime > 8) {
+                System.out.println("I dont like this fact! Sorry)");
+                student.DayStudyTime = 8;
+            } else {
+                System.out.println("Your sleeping time is OK!");
+            }
+        }
+    public void freeTimeOfStudent(StudentQAguru student){
+        if ((student.DayStudyTime + student.DayWorkTime + student.DaySleepTime) <24) {
+            System.out.println("I have good news: we go to the bar tonight!)))");
+        } else {
+            System.out.println("Life is so hard... Be strong buddy!");
+        }
+    }
+    }
